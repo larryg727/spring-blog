bbfaf945a327c445ff3da38376a316abf2f0b179
larryg727 <larrygonzales727@gmail.com>
2017-06-21 16:16:46 -0500

    -Created an interface for posts that extends CrudRepository

---
+++ src/main/java/com/codeup/Repositories/PostsRepository.java
@@ -0,0 +1,11 @@
+package com.codeup.Repositories;
+
+import com.codeup.Model.Post;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by larryg on 6/21/17.
+ */
+public interface PostsRepository extends CrudRepository<Post, Long> {
+
+}
