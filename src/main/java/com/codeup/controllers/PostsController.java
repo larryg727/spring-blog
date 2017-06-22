283f5199550f6be1595810db3c37cb1dc9cb8f6d
larryg727 <larrygonzales727@gmail.com>
2017-06-22 14:21:44 -0500

    updated delete function to a post mapping for security

--- src/main/java/com/codeup/controllers/PostsController.java
+++ src/main/java/com/codeup/controllers/PostsController.java
@@ -6,33 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by larryg on 6/19/17.
  */
 @Controller
 public class PostsController {
-    private PostSvc postDao;
+    private PostSvc postSvc;
 
     @Autowired
-    public PostsController(PostSvc postDao) {
-        this.postDao = postDao;
+    public PostsController(PostSvc postSvc) {
+        this.postSvc = postSvc;
     }
 
     @GetMapping("/posts")
     public String showAll(Model model) {
-        Iterable<Post> postList = postDao.findAll();
+        Iterable<Post> postList = postSvc.findAll();
         model.addAttribute("posts", postList);
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public  String showPost(@PathVariable long id, Model model) {
-        Post post = postDao.findOne(id);
+        Post post = postSvc.findOne(id);
         model.addAttribute("post", post);
         return "posts/show";
     }
@@ -49,31 +45,27 @@ public class PostsController {
             @RequestParam(name="body") String body
     ) {
         Post post = new Post(title, body);
-        postDao.save(post);
+        postSvc.save(post);
         return "redirect:/posts";
     }
 
     @GetMapping("/posts/{id}/edit")
     public String showEdit(@PathVariable long id, Model model) {
-        Post post = postDao.findOne(id);
+        Post post = postSvc.findOne(id);
         model.addAttribute("post", post);
         return "posts/edit";
     }
 
     @PostMapping("/posts/edit")
-    public String edit(
-            @RequestParam(name="title") String title,
-            @RequestParam(name="body") String body,
-            @RequestParam(name="id") long id
-    ) {
-        Post post = new Post(title, body, id);   //creating new post from edit for current testing purposes
-        postDao.save(post);
+    public String edit(@ModelAttribute Post post) {
+        postSvc.save(post);
         return "redirect:/posts";
     }
 
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable long id) {
-        postDao.deletePost(id);
+    @PostMapping("/posts/delete")
+    public String delete(@RequestParam long id) {
+        System.out.println(id);
+        postSvc.deletePost(id);
         return "redirect:/posts";
     }
 
