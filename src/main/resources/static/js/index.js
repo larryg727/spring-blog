b9f480741595ae0a24e5588abee74f0266c341e0
larryg727 <larrygonzales727@gmail.com>
2017-06-29 22:55:05 -0500

    *made site mobile responsive *replaced hero image with copy with lower file size to improve load time

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,22 +5,26 @@
 $(document).ready(function () {
     "use strict";
 
-var request = $.ajax({
-   url: "/posts.json"
-});
-request.done(function(){
-    console.log(request.responseJSON);
+// var request = $.ajax({
+//    url: "/posts.json"
+// });
+// request.done(function(){
+//     console.log(request.responseJSON);
+//
+//     request.responseJSON.forEach(function(post){
+//         var html = "";
+//         html += "<div>";
+//         html += "<h2>Title: " + post.title;
+//         html += "<h3>" + post.body + "</h3>";
+//         html+= "<p>Posted by: " +  post.user.username + "</p>";
+//         html+= "<p>Email: " +  post.user.email + "</p>";
+//         html += "</div>";
+//         $("#posts").append(html);
+//     });
+// });
 
-    request.responseJSON.forEach(function(post){
-        var html = "";
-        html += "<div>";
-        html += "<h2>Title: " + post.title;
-        html += "<h3>" + post.body + "</h3>";
-        html+= "<p>Posted by: " +  post.user.username + "</p>";
-        html+= "<p>Email: " +  post.user.email + "</p>";
-        html += "</div>";
-        $("#posts").append(html);
-    });
+$('#menuHam').click(function(){
+    $('nav button').slideToggle();
 });
 
 
